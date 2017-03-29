fc2ef235abcbbb04730b62d175e00ec45a1c782b
Kunchao <739136321@qq.com>
2017-03-29 17:10:16 +0800

    Update best_collages.py

--- best_collages.py
+++ best_collages.py
@@ -12,14 +12,6 @@ def getHTMLText(url):
 	except:
 		return ""
 
-# def getHTMLText(url):
-#     try:
-#         r = requests.get(url, timeout=30)
-#         r.raise_for_status()
-#         r.encoding = r.apparent_encoding
-#         return r.text
-#     except:
-#         return ""
 
 def fillUnivList(ulsit,html):
 	soup = BeautifulSoup(html,"lxml")
@@ -31,7 +23,7 @@ def fillUnivList(ulsit,html):
 
 
 # def fillUnivList(ulist, html):
-#     soup = BeautifulSoup(html, "html.parser")
+#     soup = BeautifulSoup(html, "html.parser")#2016
 #     for tr in soup.find('tbody').children:
 #         if isinstance(tr, bs4.element.Tag):
 #             tds = tr('td')
